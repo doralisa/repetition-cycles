507bf9acd0984d49e75c21a24afd3190ab371fdb
Doralys Contreras <doralisacr@gmail.com>
2020-08-24 13:00:04 +0100

    finish exercise one B

---
+++ src/ar/com/ada/online/second/repetitioncycles/ExerciseOneB.java
@@ -0,0 +1,23 @@
+package ar.com.ada.online.second.repetitioncycles;
+
+import java.util.Scanner;
+
+public class ExerciseOneB {
+
+    public static void main(String[] args) {
+
+        int resultSum = 0;
+        int[] arrayNumbers = new int[10];
+
+        Scanner keyboard = new Scanner(System.in);
+
+        for (int i = 0; i < arrayNumbers.length; i++) {
+            System.out.printf("Ingrese el valor para la posición %d del arreglo: ", i);
+            arrayNumbers[i] = keyboard.nextInt();
+            resultSum = resultSum + arrayNumbers[i];
+        }
+
+        System.out.printf("La suma de los elementos del arreglo es: %d", resultSum);
+
+    }
+}
