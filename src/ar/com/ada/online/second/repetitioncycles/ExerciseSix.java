c9d8323ab5a157baa84abd277ad476451ed8a9db
Doralys Contreras <doralisacr@gmail.com>
2020-08-24 20:52:18 +0100

    finish exercise six

---
+++ src/ar/com/ada/online/second/repetitioncycles/ExerciseSix.java
@@ -0,0 +1,29 @@
+package ar.com.ada.online.second.repetitioncycles;
+
+import java.util.Scanner;
+
+public class ExerciseSix {
+
+    public static void main(String[] args) {
+        int [][] matriz = new int [4][4];
+        int zeroCounter = 0;
+        Scanner keyboard = new Scanner(System.in);
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.printf("Ingrese el valor para la matriz en la posición %d, %d ", i, j ": ");
+                matriz [i][j] = keyboard.nextInt();
+            }
+        }
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] == 0)
+                    zeroCounter = zeroCounter + 1;
+            }
+        }
+
+        System.out.printf("La cantidad de ceros en la matriz es: %d", zeroCounter);
+
+    }
+}
