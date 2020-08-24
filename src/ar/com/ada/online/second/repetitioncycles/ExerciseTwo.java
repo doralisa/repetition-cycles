62e43dd9728a081b1beb9133dd035a83021166c8
Doralys Contreras <doralisacr@gmail.com>
2020-08-24 15:32:36 +0100

    finish exercise two

---
+++ src/ar/com/ada/online/second/repetitioncycles/ExerciseTwo.java
@@ -0,0 +1,40 @@
+package ar.com.ada.online.second.repetitioncycles;
+
+import java.util.Scanner;
+
+public class ExerciseTwo {
+
+    public static void main(String[] args) {
+
+        int[] arrayA;
+        int[] arrayB;
+        int[] arrayC;
+        int sizeArray;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Ingrese el tamaño de los arreglos: ");
+        sizeArray = keyboard.nextInt();
+
+        arrayA = new int[sizeArray];
+        arrayB = new int[sizeArray];
+        arrayC = new int[sizeArray];
+
+        for (int i = 0; i < sizeArray; i++) {
+            System.out.print("Ingrese el valor para el arreglo A en la posición " + i + ": ");
+            arrayA[i] = keyboard.nextInt();
+
+            System.out.print("Ingrese el valor para el arreglo B en la posición " + i + ": ");
+            arrayB[i] = keyboard.nextInt();
+        }
+
+        for (int i = 0; i < arrayA.length; i++) {
+            arrayC[i] = arrayA[i] + arrayB[i];
+        }
+
+        System.out.println("EL resultado de la suma de los elementos de los arreglos A y B es: ");
+        for (int i = 0; i < arrayB.length; i++) {
+            System.out.printf("indice: %d, valor: %d\n", i, arrayC[i]);
+        }
+    }
+}
